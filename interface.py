fb3c14c3a0329650bd869f8d20d18a8601d2928a
CriezelMalabanan <113971162+CriezelMalabanan@users.noreply.github.com>
2022-12-08 07:56:01 +0700

    Update interface.py

--- interface.py
+++ interface.py
@@ -13,7 +13,7 @@ def sign_up():
         print("Invalid Amount")
         return
     
-    pin = int(input('Create PIN: '))
+    pin = int(input("Create PIN => "))
     customer = BankAccount(name=name, mobile_no=mobile_no, initial_depo=initial_depo, pin=pin)
     customer_dict[customer.cust_acc_num] = customer                 # acct. no. stored as key and oject as value
     mobile_acc_link[customer.mobile_no] = customer.cust_acc_num     # mobile no. linked
@@ -29,5 +29,29 @@ def login():
     if account_no in customer_dict.keys() and account_pin == customer_dict[account_no].pin :
         print(f"\n\t>>>>> {customer_dict[account_no].name} CONGRATS! Successfully Logged In! <<<<")
         customer_dict[account_no].basic_details()
+    else:
+        print("\n\t>>>>>> Incorrect Account Number or Pin!! <<<<<<")
+        return
+    while True:
+        print("***********************************************************")
+        print("|\t\t\t\t\tWelcome to Our Bank\t\t\t\t\t  |")
+        print("***********************************************************")
+        print("\n\t========= Choose what you want to do in Our Bank =========")
+        
+        print("\t\t\t\t1. Deposit")
+        print("\t\t\t\t2. Withdraw")
+        print("\t\t\t\t3. Money Transfer ")
+        print("\t\t\t\t4. Logout")
+        user_input1 = input("Please Enter your Choice =>  ")
+
+        if user_input1 == '1':
+            customer_dict[account_no].deposit()
+        elif user_input1 == '2':
+            customer_dict[account_no].withdrawl()
+        elif user_input1 == '3':
+            print("\n***********************************************************")
+            print("|\t\t\t\t\t\tMoney Transfer\t\t\t\t\t  |")
+            print("***********************************************************")
+        
     
         
